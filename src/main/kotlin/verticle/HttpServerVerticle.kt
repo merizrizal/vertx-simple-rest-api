feb57b8b4a9c1b59bdd77c5f7048aa361d9c5be3
merizrizal <meriz.rizal@gmail.com>
2022-06-29 14:52:10 +0700

    create update method

--- src/main/kotlin/verticle/HttpServerVerticle.kt
+++ src/main/kotlin/verticle/HttpServerVerticle.kt
@@ -1,6 +1,9 @@
 package verticle
 
 import config.Config
+import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Single
+import io.vertx.core.Future
 import io.vertx.core.Promise
 import io.vertx.core.json.JsonObject
 import io.vertx.redis.client.RedisOptions
@@ -8,33 +11,17 @@ import io.vertx.rxjava3.core.AbstractVerticle
 import io.vertx.rxjava3.ext.web.Router
 import io.vertx.rxjava3.ext.web.RoutingContext
 import io.vertx.rxjava3.ext.web.handler.BodyHandler
-import io.vertx.rxjava3.pgclient.PgPool
 import io.vertx.rxjava3.redis.client.Redis
 import io.vertx.rxjava3.redis.client.RedisAPI
 
 class HttpServerVerticle : AbstractVerticle() {
-    private lateinit var redisApi: RedisAPI
+    private lateinit var redis: Redis
 
     override fun start(promise: Promise<Void>) {
         val redisOptions = RedisOptions()
             .setConnectionString("redis://:${Config.REDIS_PASSWORD}@${Config.REDIS_HOST}/${Config.REDIS_DATABASE}")
 
-        Redis(io.vertx.redis.client.Redis.createClient(vertx.delegate, redisOptions))
-            .rxConnect()
-            .subscribe(
-                { redisConnection ->
-                    redisConnection.handler {
-                        println(it)
-                        if (it.size() > 0) {
-                            if (it[0].toString() == "message") {
-                                println("Received message at channel ${it[1]}: ${it[2]}")
-                            }
-                        }
-                    }
-
-                    redisApi = RedisAPI.api(redisConnection)
-                },
-                { failure -> promise.fail(failure.cause) })
+        redis = Redis(io.vertx.redis.client.Redis.createClient(vertx.delegate, redisOptions))
 
         val router = Router.router(vertx).apply {
             get("/api/users").handler(this@HttpServerVerticle::getUsers)
@@ -55,15 +42,6 @@ class HttpServerVerticle : AbstractVerticle() {
     private fun getUsers(context: RoutingContext) {
         var response: JsonObject
 
-//        for (row in rows) {
-//            data.add(JsonObject().apply {
-//                put("user_id", row.getString("user_id"))
-//                put("user_name", row.getString("user_name"))
-//                put("name_alias", row.getString("name_alias"))
-//                put("company", row.getString("company"))
-//            })
-//        }
-//
         response = JsonObject().apply {
             put("success", true)
             put("data", true)
@@ -80,12 +58,51 @@ class HttpServerVerticle : AbstractVerticle() {
         val nameAlias = context.request().getParam("name_alias")
         val company = context.request().getParam("company")
 
-        response = JsonObject().apply {
-            put("success", false)
-            put("error", true)
-        }
+        redis
+            .rxConnect()
+            .subscribe(
+                { redisConnection ->
+                    RedisAPI.api(redisConnection)
+                        .rxHmset(ArrayList<String>().apply {
+                            add("heroes:${userId}")
+                            add("userName")
+                            add(userName)
+                            add("nameAlias")
+                            add(nameAlias)
+                            add("company")
+                            add(company)
+                        })
+                        .subscribe(
+                            {
+                                response = JsonObject().apply {
+                                    put("success", true)
+                                    put("action", "insert")
+                                }
 
-        putResponse(context, 500, response)
+                                putResponse(context, 200, response)
+
+                                redisConnection.rxClose()
+                            },
+                            {
+                                response = JsonObject().apply {
+                                    put("success", false)
+                                    put("message", it.message)
+                                }
+
+                                putResponse(context, 500, response)
+
+                                redisConnection.rxClose()
+
+                            })
+                },
+                {
+                    response = JsonObject().apply {
+                        put("success", false)
+                        put("message", it.message)
+                    }
+
+                    putResponse(context, 500, response)
+                })
     }
 
     private fun updateUser(context: RoutingContext) {
@@ -95,6 +112,83 @@ class HttpServerVerticle : AbstractVerticle() {
         val userName = context.request().getParam("user_name")
         val nameAlias = context.request().getParam("name_alias")
         val company = context.request().getParam("company")
+
+        redis
+            .rxConnect()
+            .subscribe(
+                { redisConnection ->
+                    val redisApi = RedisAPI.api(redisConnection)
+                    redisApi
+                        .rxHmget(ArrayList<String>().apply {
+                            add("heroes:${userId}")
+                            add("userName")
+                        })
+                        .toObservable()
+                        .map {
+                            null != it.iterator().next()
+                        }
+                        .flatMapMaybe { exist ->
+                            if (exist) {
+                                redisApi
+                                    .rxHmset(ArrayList<String>().apply {
+                                        add("heroes:${userId}")
+
+                                        if (null != userName) {
+                                            add("userName")
+                                            add(userName)
+                                        }
+
+                                        if (null != nameAlias) {
+                                            add("nameAlias")
+                                            add(nameAlias)
+                                        }
+
+                                        if (null != company) {
+                                            add("company")
+                                            add(company)
+                                        }
+                                    })
+                            } else {
+                                Maybe.just(false)
+                            }
+                        }
+                        .subscribe(
+                            {
+                                response = if (it is Boolean && !it) {
+                                    JsonObject().apply {
+                                        put("success", false)
+                                        put("message", "Data doesn't exist")
+                                    }
+                                } else {
+                                    JsonObject().apply {
+                                        put("success", true)
+                                        put("action", "update")
+                                    }
+                                }
+
+                                putResponse(context, 200, response)
+
+                                redisConnection.rxClose()
+                            },
+                            {
+                                response = JsonObject().apply {
+                                    put("success", false)
+                                    put("message", it.message)
+                                }
+
+                                putResponse(context, 500, response)
+
+                                redisConnection.rxClose()
+                            })
+                },
+                {
+                    response = JsonObject().apply {
+                        put("success", false)
+                        put("message", it.message)
+                    }
+
+                    putResponse(context, 500, response)
+                })
     }
 
     private fun deleteUser(context: RoutingContext) {

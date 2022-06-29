d5c90fa85416575e64edd7a1b5c1f76b559782f9
merizrizal <meriz.rizal@gmail.com>
2022-06-29 09:31:41 +0700

    Setup redis

--- src/main/kotlin/verticle/HttpServerVerticle.kt
+++ src/main/kotlin/verticle/HttpServerVerticle.kt
@@ -2,34 +2,39 @@ package verticle
 
 import config.Config
 import io.vertx.core.Promise
-import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
-import io.vertx.pgclient.PgConnectOptions
+import io.vertx.redis.client.RedisOptions
 import io.vertx.rxjava3.core.AbstractVerticle
 import io.vertx.rxjava3.ext.web.Router
 import io.vertx.rxjava3.ext.web.RoutingContext
 import io.vertx.rxjava3.ext.web.handler.BodyHandler
 import io.vertx.rxjava3.pgclient.PgPool
-import io.vertx.rxjava3.sqlclient.Tuple
-import io.vertx.sqlclient.PoolOptions
+import io.vertx.rxjava3.redis.client.Redis
+import io.vertx.rxjava3.redis.client.RedisAPI
 
 class HttpServerVerticle : AbstractVerticle() {
-    private lateinit var pgPoolClient: PgPool
+    private lateinit var redisApi: RedisAPI
 
     override fun start(promise: Promise<Void>) {
-        val pgConnectOptions = PgConnectOptions()
-            .setPort(5432)
-            .setHost(Config.PG_HOST)
-            .setDatabase(Config.PG_DATABASE)
-            .setUser(Config.PG_USER)
-            .setPassword(Config.PG_PASSWORD)
+        val redisOptions = RedisOptions()
+            .setConnectionString("redis://:${Config.REDIS_PASSWORD}@${Config.REDIS_HOST}/${Config.REDIS_DATABASE}")
 
-        val poolOptions = PoolOptions().setMaxSize(5)
+        Redis(io.vertx.redis.client.Redis.createClient(vertx.delegate, redisOptions))
+            .rxConnect()
+            .subscribe(
+                { redisConnection ->
+                    redisConnection.handler {
+                        println(it)
+                        if (it.size() > 0) {
+                            if (it[0].toString() == "message") {
+                                println("Received message at channel ${it[1]}: ${it[2]}")
+                            }
+                        }
+                    }
 
-        pgPoolClient = PgPool.newInstance(io.vertx.pgclient.PgPool.pool(
-            vertx.delegate,
-            pgConnectOptions,
-            poolOptions))
+                    redisApi = RedisAPI.api(redisConnection)
+                },
+                { failure -> promise.fail(failure.cause) })
 
         val router = Router.router(vertx).apply {
             get("/api/users").handler(this@HttpServerVerticle::getUsers)
@@ -50,53 +55,21 @@ class HttpServerVerticle : AbstractVerticle() {
     private fun getUsers(context: RoutingContext) {
         var response: JsonObject
 
-        pgPoolClient.rxGetConnection()
-            .subscribe(
-                { sqlConnection ->
-                    sqlConnection.query("SELECT * FROM heroes")
-                        .rxExecute()
-                        .subscribe(
-                            { rows ->
-                                val data = JsonArray()
+//        for (row in rows) {
+//            data.add(JsonObject().apply {
+//                put("user_id", row.getString("user_id"))
+//                put("user_name", row.getString("user_name"))
+//                put("name_alias", row.getString("name_alias"))
+//                put("company", row.getString("company"))
+//            })
+//        }
+//
+        response = JsonObject().apply {
+            put("success", true)
+            put("data", true)
+        }
 
-                                for (row in rows) {
-                                    data.add(JsonObject().apply {
-                                        put("user_id", row.getString("user_id"))
-                                        put("user_name", row.getString("user_name"))
-                                        put("name_alias", row.getString("name_alias"))
-                                        put("company", row.getString("company"))
-                                    })
-                                }
-
-                                response = JsonObject().apply {
-                                    put("success", true)
-                                    put("data", data)
-                                }
-
-                                putResponse(context, 200, response)
-
-                                sqlConnection.close()
-                            },
-                            {
-                                response = JsonObject().apply {
-                                    put("success", false)
-                                    put("error", it.message)
-                                }
-
-                                putResponse(context, 500, response)
-
-                                sqlConnection.close()
-                            })
-                },
-                {
-                    response = JsonObject().apply {
-                        put("success", false)
-                        put("error", it.message)
-                    }
-
-                    putResponse(context, 500, response)
-                }
-            )
+        putResponse(context, 200, response)
     }
 
     private fun setUser(context: RoutingContext) {
@@ -107,43 +80,12 @@ class HttpServerVerticle : AbstractVerticle() {
         val nameAlias = context.request().getParam("name_alias")
         val company = context.request().getParam("company")
 
-        pgPoolClient.rxGetConnection()
-            .subscribe(
-                { sqlConnection ->
-                    sqlConnection.preparedQuery("INSERT INTO heroes(user_id, user_name, name_alias, company) " +
-                            "VALUES ($1, $2, $3, $4)")
-                        .rxExecute(Tuple.of(userId, userName, nameAlias, company))
-                        .subscribe(
-                            {
-                                response = JsonObject().apply {
-                                    put("success", true)
-                                    put("action", "insert")
-                                }
+        response = JsonObject().apply {
+            put("success", false)
+            put("error", true)
+        }
 
-                                putResponse(context, 200, response)
-
-                                sqlConnection.close()
-                            },
-                            {
-                                response = JsonObject().apply {
-                                    put("success", false)
-                                    put("error", it.message)
-                                }
-
-                                putResponse(context, 500, response)
-
-                                sqlConnection.close()
-                            })
-                },
-                {
-                    response = JsonObject().apply {
-                        put("success", false)
-                        put("error", it.message)
-                    }
-
-                    putResponse(context, 500, response)
-                }
-            )
+        putResponse(context, 500, response)
     }
 
     private fun updateUser(context: RoutingContext) {
@@ -153,88 +95,12 @@ class HttpServerVerticle : AbstractVerticle() {
         val userName = context.request().getParam("user_name")
         val nameAlias = context.request().getParam("name_alias")
         val company = context.request().getParam("company")
-
-        pgPoolClient.rxGetConnection()
-            .subscribe(
-                { sqlConnection ->
-                    sqlConnection.preparedQuery("UPDATE heroes " +
-                            "SET user_name=$1, name_alias=$2, company=$3 " +
-                            "WHERE user_id=$4")
-                        .rxExecute(Tuple.of(userName, nameAlias, company, userId))
-                        .subscribe(
-                            {
-                                response = JsonObject().apply {
-                                    put("success", true)
-                                    put("action", "update")
-                                }
-
-                                putResponse(context, 200, response)
-
-                                sqlConnection.close()
-                            },
-                            {
-                                response = JsonObject().apply {
-                                    put("success", false)
-                                    put("error", it.message)
-                                }
-
-                                putResponse(context, 500, response)
-
-                                sqlConnection.close()
-                            })
-                },
-                {
-                    response = JsonObject().apply {
-                        put("success", false)
-                        put("error", it.message)
-                    }
-
-                    putResponse(context, 500, response)
-                }
-            )
     }
 
     private fun deleteUser(context: RoutingContext) {
         var response: JsonObject
 
         val userId = context.request().getParam("user_id")
-
-        pgPoolClient.rxGetConnection()
-            .subscribe(
-                { sqlConnection ->
-                    sqlConnection.preparedQuery("DELETE FROM heroes WHERE user_id=$1")
-                        .rxExecute(Tuple.of(userId))
-                        .subscribe(
-                            {
-                                response = JsonObject().apply {
-                                    put("success", true)
-                                    put("action", "delete")
-                                }
-
-                                putResponse(context, 200, response)
-
-                                sqlConnection.close()
-                            },
-                            {
-                                response = JsonObject().apply {
-                                    put("success", false)
-                                    put("error", it.message)
-                                }
-
-                                putResponse(context, 500, response)
-
-                                sqlConnection.close()
-                            })
-                },
-                {
-                    response = JsonObject().apply {
-                        put("success", false)
-                        put("error", it.message)
-                    }
-
-                    putResponse(context, 500, response)
-                }
-            )
     }
 
     private fun putResponse(context: RoutingContext, statuscode: Int, response: JsonObject) {
